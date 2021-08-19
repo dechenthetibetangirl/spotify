f48e280594bfda10dc8252a79ddf4c2a6192a5b3
Dechen wangmo Manckang <56068560+dechenthetibetangirl@users.noreply.github.com>
2021-08-19 10:07:20 +0200

    second commit

--- types.tsx
+++ types.tsx
@@ -8,15 +8,35 @@ export type RootStackParamList = {
   NotFound: undefined;
 };
 
+
 export type BottomTabParamList = {
   TabOne: undefined;
   TabTwo: undefined;
+ 
 };
 
 export type TabOneParamList = {
   TabOneScreen: undefined;
+
 };
 
 export type TabTwoParamList = {
   TabTwoScreen: undefined;
 };
+
+export type Album= {
+
+  id: string;
+  imageUri: string;
+  name:string;
+  by: string, 
+  numberOfLikes: number,
+  artistsHeadline: string;
+}
+
+export type Song ={
+  id: string,
+  imageUri:string,
+  title: string,
+  artist: string,
+}
